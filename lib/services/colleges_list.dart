b6010fdd228d7010d71559b7f74f19a22964f628
Daniel Chukwu <00danzy@gmail.com>
2022-07-12 14:00:14 +0100

    create boilerplate for CollegesList object

---
+++ lib/services/colleges_list.dart
@@ -0,0 +1,11 @@
+class CollegesList {
+  late String country;
+  late Map colleges;
+  late bool validCountry;
+
+  CollegesList({required this.country, required this.colleges});
+
+  void getColleges() {
+    // get Colleges logic should be here
+  }
+}
