e9a2ff675c2b3d21509d8c441e6f34a0c664d94a
Daniel Chukwu <00danzy@gmail.com>
2022-07-12 11:26:35 +0100

    add new background color

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,10 +9,25 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF00acc0),
       body: Center(
         child: Column(
-          children: [
-            Text('FindCollege'),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'FindCollege',
+              style: TextStyle(
+                fontSize: 40,
+                letterSpacing: 2,
+                color: Colors.white,
+                fontFamily: 'DMSerif',
+              ),
+            ),
+            SizedBox(height: 30.0),
+            SpinKitFoldingCube(
+              color: Colors.white,
+              size: 40,
+            )
           ],
         ),
       ),
